218f4830bce53714b016478b5f9e3bad43111ee2
mini-gui <guido.minieri@gmail.com>
2017-12-25 11:57:00 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # caesar-cypher
 A commandline tool that allows you to decrypt your messages using the Ceasar Cypher
+
+# usage
+The following parameters are allowed
+"-r" - change the base to decrypt the message
---
+++ caesar.py
@@ -0,0 +1,18 @@
+# caesar-cypher.py - a command line tool to decrypt your messages
+# Created by: mini-gui
+# Date: December 2017
+
+
+import sys
+
+
+
+
+# TODO
+# run the program by default to use the classical -3 shift
+
+# run the program from the command line with -c flag and then option to change the shift size
+
+# promp inside the program the shift 
+
+# allow encrypt and decrypt change

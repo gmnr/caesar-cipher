85c86ee563f337e40c44e783a3162bd0153d72fb
mini-gui <guido.minieri@gmail.com>
2017-12-26 19:57:10 +0100

    added encrypt file functionality

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -6,12 +6,12 @@
 # imports
 import sys
 import argparse
-import os
 from time import sleep
+
 
 # functions
 def shift(index, key):
-    """returns the modified index in recursive base 25"""
+    """Returns the modified index in recursive base 25"""
     if key >= 0:
         for i in range(key):
             index += 1
@@ -27,13 +27,16 @@ def shift(index, key):
 
 
 def crypt(message):
-    """given a message returns its encrypted version"""
+    """Given a message returns its encrypted version"""
     crypted_msg = ''
     global crypt_key
 
     for word in message.split(' '):
         crypted_word = ''
         for letter in word:
+            if letter not in crypt_base:     # keep special characters in place
+                crypted_word += letter
+                continue
             crypted_word += crypt_base[shift(crypt_base.index(letter), crypt_key)]
         crypted_msg += crypted_word + ' '
     crypted_msg = crypted_msg[:-1]
@@ -42,12 +45,21 @@ def crypt(message):
 
 
 def crypt_file(path):
-    """Encrypt the selected file"""
-    with open(path, 'w') as f:
+    """Encrypts the selected file"""
+    buffer_message = []
+    with open(path, 'r+') as f:
         lines = f.readlines()
         for line in lines:
-            line = crypt(line)
-            f.write(line)
+            if line == '\n':
+                buffer_message.append(line)
+                continue
+            line = crypt(line[:-1])    # remove newline at the end
+            buffer_message.append(line)
+    with open(path, 'w') as output:
+        for line in buffer_message:
+            output.write(line)
+
+    return None
 
 
 # Parser settings
@@ -77,11 +89,22 @@ if __name__ == '__main__':
 # if the '-p' flag is true promtp to a file location
         if arguments.file_path:
             path_to_file = input('Select the file you want to encrypt\n')
-            crypt_file(path_to_file)
+# if multiple flags '-pk'
+            if arguments.key_stored != None:
+                crypt_key = int(arguments.key_stored)
+                crypt_file(path_to_file)
+                print('Conversion completed')
+# if only with '-p'
+            else:
+                crypt_key = int(input('What is the key?\n')) 
+                crypt_file(path_to_file)
+                print('Conversion completed')
+
 
 # else use the '-k' flag to encrypt a message
-        crypt_key = int(arguments.key_stored)
-        msg = input('Input the message you need to encrypt..\n').lower()
-        print(2*'\n')
-        sleep(0.2)
-        print(crypt(msg))
+        else:
+            crypt_key = int(arguments.key_stored)
+            msg = input('Input the message you need to encrypt..\n').lower()
+            print(2*'\n')
+            sleep(0.2)
+            print(crypt(msg))
